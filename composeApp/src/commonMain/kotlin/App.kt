5ebf7a11385fbe41deef43f8413479bc7a293d2a
frank <frank.bommeli@mimacom.com>
2024-02-09 18:37:57 +0100

    layout some

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,18 +1,17 @@
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import kotlinx.datetime.*
+import kotlinx.datetime.Clock
+import kotlinx.datetime.LocalTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 
@@ -22,25 +21,43 @@ fun App() {
     MaterialTheme {
         var selectedCard: Card? by remember { mutableStateOf(null) }
 
-        Text(selectedCard?.value ?: "No card selected")
         Column(modifier = Modifier.padding(20.dp)) {
+            Text(
+                selectionText(selectedCard),
+                style = TextStyle(fontSize = 20.sp),
+                textAlign = TextAlign.Center,
+                modifier = Modifier.fillMaxWidth().align(Alignment.CenterHorizontally)
+            )
 
             Row(modifier = Modifier.padding(start = 20.dp, top = 10.dp)) {
-                Button(modifier = Modifier.padding(top = 10.dp),
-                    onClick = {selectedCard = Cards.one}) {
-                    card(Cards.one)
+
+                cards().forEach {
+                    Button(modifier = Modifier.padding(all = 10.dp),
+                        onClick = { selectedCard = it }) {
+                        card(it)
+                    }
                 }
             }
         }
     }
 }
 
+private fun selectionText(selectedCard: Card?) =
+    if (selectedCard?.value == null) "Nothing selected " else "Your selection ${selectedCard.value}"
+
 @Composable
 fun card(card: Card) {
     Text(card.value)
 }
 
-object Cards{val one: Card = Card("1")}
+fun cards() = listOf(
+    Card("1"),
+    Card("2"),
+    Card("3"),
+    Card("5"),
+    Card("8"),
+    Card("13"),
+)
 
 data class Card(val value: String)
 
